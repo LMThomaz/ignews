57e019992520e5abed093c380dc07c13c371141c
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-27 19:19:11 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,30 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de marlo de 2021</time>
-            <strong>Creating a Monorepo with Lenna & Yarn Workspace</strong>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-              fugiat eius temporibus iusto tempore.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de marlo de 2021</time>
-            <strong>Creating a Monorepo with Lenna & Yarn Workspace</strong>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-              fugiat eius temporibus iusto tempore.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de marlo de 2021</time>
-            <strong>Creating a Monorepo with Lenna & Yarn Workspace</strong>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-              fugiat eius temporibus iusto tempore.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -54,9 +49,25 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
