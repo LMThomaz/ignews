c72d159ccb7ecaa3e8af8358e3af6f3788021e8a
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-27 14:36:11 -0300

    Ouvindo mais eventos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,16 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt='ig.news' />
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href='#' className={styles.active}>
+          <a href="/" className={styles.active}>
             Home
           </a>
-          <a href='#'>Posts</a>
+          <a href="#">Posts</a>
         </nav>
         <SignInButton />
       </div>
