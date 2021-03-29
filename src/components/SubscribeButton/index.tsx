42292d89bd0042cbc98b441ecdafca203d1a1c7d
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-29 19:15:28 -0300

    Validando assinatura ativa

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,8 @@
-import { signIn, useSession } from "next-auth/client";
-import { api } from "../../services/api";
-import { getStripeJs } from "../../services/stripe-js";
-import styles from "./styles.module.scss";
+import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -9,15 +10,21 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
-      signIn("github");
+      signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
     try {
-      const response = await api.post("/subscribe");
+      const response = await api.post('/subscribe');
 
       const { sessionId } = response.data;
 
