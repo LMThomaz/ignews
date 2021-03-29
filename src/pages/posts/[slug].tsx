c5a59fd98f998d10d1e5f14b229fa64b235e1de8
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-29 19:34:56 -0300

    Página: Preview do post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,10 +1,10 @@
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/client";
-import Head from "next/head";
-import { RichText } from "prismic-dom";
-import { getPrismicClient } from "../../services/prismic";
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/client';
+import Head from 'next/head';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../services/prismic';
 
-import styles from "./post.module.scss";
+import styles from './post.module.scss';
 
 interface PostProps {
   post: {
@@ -43,10 +43,10 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const { slug } = params;
 
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
-        destination: "/",
+        destination: '/',
         permanent: false,
       },
     };
@@ -54,18 +54,18 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   const prismic = getPrismicClient(req);
 
-  const response = await prismic.getByUID("posts", String(slug), {});
+  const response = await prismic.getByUID('posts', String(slug), {});
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString(
-      "pt-BR",
+      'pt-BR',
       {
-        day: "2-digit",
-        month: "long",
-        year: "numeric",
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
       }
     ),
   };
