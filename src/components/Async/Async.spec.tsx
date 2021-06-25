79c28aafcfac83f71bd590b559b673a7acd26975
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-06-25 13:34:21 -0300

    Testing playground

--- src/components/Async/Async.spec.tsx
+++ src/components/Async/Async.spec.tsx
@@ -10,11 +10,6 @@ test('it renders correctly', async () => {
   render(<Async />);
 
   expect(screen.getByText('Hello world!')).toBeInTheDocument();
-  expect(await screen.findByText('Button')).toBeInTheDocument();
 
   await waitForElementToBeRemoved(screen.queryByText('Button'));
-
-  await waitFor(() => {
-    return expect(screen.queryByText('Button')).not.toBeInTheDocument();
-  });
 });
