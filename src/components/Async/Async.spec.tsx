2145aa83fb37b85fa3b9adb589a9dd766dea275d
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-06-25 13:25:15 -0300

    Dicas de testes assíncronos

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,20 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { Async } from '.';
+
+test('it renders correctly', async () => {
+  render(<Async />);
+
+  expect(screen.getByText('Hello world!')).toBeInTheDocument();
+  expect(await screen.findByText('Button')).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(screen.queryByText('Button'));
+
+  await waitFor(() => {
+    return expect(screen.queryByText('Button')).not.toBeInTheDocument();
+  });
+});
---
+++ src/components/Async/index.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+export function Async() {
+  const [isButtonInvisible, setIsButtonInvisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsButtonInvisible(true);
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      <div>Hello world!</div>
+
+      {!isButtonInvisible && <button>Button</button>}
+    </div>
+  );
+}
